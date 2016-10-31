2d6e7adb5e5fe377e20d3fcfb3cbce663c446f24
Benjamin Cooper <benjaminkagancooper@gmail.com>
2016-10-31 17:59:58 -0400

    update

--- pages/contactMe.js
+++ pages/contactMe.js
@@ -6,6 +6,10 @@ import { config } from 'config'
 import NavBar from '../components/NavBar'
 import WelcomePageCenter from '../components/WelcomPageCenter'
 
+// <div>Research Email: <a href="mailto:benjamin.k.cooper.18@dartmouth.edu">benjamin.k.cooper.18@dartmouth.edu</a></div>
+// <div>Work Email: <a href="mailto:benjaminkagancooper@gmail.com">benjaminkagancooper@gmail.com</a></div>
+// <img id="contactMe-sub" src={prefixLink('./photo.jpg')} width="15%" />
+
 export default class Index extends React.Component {
   render () {
     return (
@@ -14,10 +18,7 @@ export default class Index extends React.Component {
           <NavBar />
             <div id="contactMe-center">
               <br />
-              <img id="contactMe-sub" src="./photo.jpg" width="15%" />
               <h1>Benjamin Cooper</h1>
-              <div>Research Email: <a href="mailto:benjamin.k.cooper.18@dartmouth.edu">benjamin.k.cooper.18@dartmouth.edu</a></div>
-              <div>Work Email: <a href="mailto:benjaminkagancooper@gmail.com">benjaminkagancooper@gmail.com</a></div>
           </div>
         </div>
       </DocumentTitle>
